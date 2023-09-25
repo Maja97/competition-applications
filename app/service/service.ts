efe576c61494867cf52a5b1c9a54ff1543686d6b
mmatijevic <mmatijevic@collectivemind.dev>
2023-09-25 11:59:20 +0200

    Base url changed

--- app/service/service.ts
+++ app/service/service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: 'https://elevien-fe-job.free.beeceptor.com',
   timeout: 25 * 1000
 });
